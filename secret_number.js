f7983fa7d6dead60409cd50cb93a88814044769e
Ella Mae A <ellamae207@gmail.com>
2017-09-13 14:36:19 -1000

    passes 3

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+  function randomNum (){
+    return ((Math.random() * 1000000 ) + 1); // RANDOM NUMBERS HAVE BEEN GENERATED
+  };
+/*STORE RANDOM NUMBERS GENERATED IN A VARIABLE, SO ONCE IT CALLS IT, IT GIVES THE SAME SET OF NUMBERS.*/
+  var numbers = randomNum();
+
+  function genNums() {
+    return numbers;
+  }
+  return genNums;
 };
+
+
